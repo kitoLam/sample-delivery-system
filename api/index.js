9a007085b7d8edfc14076b4b4017366f8ffcc315
Thangwibu <lameojicoemail@gmail.com>
2026-03-12 12:17:50 +0700

    feature add endpoint

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 // Không cần config dotenv ở đây nếu bạn đã nhập biến môi trường trên Vercel Dashboard
-require('dotenv').config(); 
-const { connectDb } = require('../config/db.config'); // Lưu ý đường dẫn nếu bạn cho vào thư mục api/
-const controller = require('../controllers/index.controller');
+require("dotenv").config();
+const { connectDb } = require("../config/db.config"); // Lưu ý đường dẫn nếu bạn cho vào thư mục api/
+const controller = require("../controllers/index.controller");
 
 const app = express();
 app.use(express.json());
@@ -11,12 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 // Kết nối DB ngay lập tức (Vercel sẽ giữ lại kết nối này giữa các request nếu có thể)
 connectDb();
 
-app.get('/', (req, res) => { res.json('ok') });
-app.post('/create-shipment', controller.createShip);
-app.post('/mark-receive/:id', controller.markReceive);
-app.post('/mark-complete/:id', controller.markComplete);
-app.post('/mark-fail/:id', controller.markFail);
-
+app.get("/", (req, res) => {
+  res.json("ok");
+});
+app.post("/create-shipment", controller.createShip);
+app.post("/mark-receive/:id", controller.markReceive);
+app.post("/mark-complete/:id", controller.markComplete);
+app.post("/mark-fail/:id", controller.markFail);
+app.get("/ships", controller.getAllShips);
 
 // QUAN TRỌNG 2: Xuất app để Vercel handler nhận diện
 module.exports = app;
